5be5f966bbd7daca0124f16a71ee036dad5f38a0
Zachary Abresch <zachary.abresch@gmail.com>
2018-06-22 16:12:39 +0000

    Add `debug`

--- src/Ogle.js
+++ src/Ogle.js
@@ -1,7 +1,11 @@
+const debug = require("debug")("Ogle");
+
 /** Class for visual diffing between domains */
 class Ogle {
   /** Creates Ogle instance */
-  constructor() {}
+  constructor() {
+    debug("constructor");
+  }
 }
 
 module.exports = Ogle;
