b3b04ffff6faa9f7f77bdd651fc06af27b21aef9
Zachary Abresch <zachary.abresch@gmail.com>
2018-06-22 18:41:26 +0000

    Implement better constructor & error handling

--- src/Ogle.js
+++ src/Ogle.js
@@ -3,9 +3,16 @@ const debug = require("debug")("Ogle");
 /** Class for visual diffing between domains */
 class Ogle {
   /** Creates Ogle instance */
-  constructor() {
-    debug("constructor");
+  constructor({ base, test } = {}) {
+    debug("constructing");
+    if (!base || !test) {
+      throw new Error("required params not provided");
+    }
+    this.base = base;
+    this.test = test;
   }
+
+  capture() {}
 }
 
 module.exports = Ogle;
--- src/Ogle.spec.js
+++ src/Ogle.spec.js
@@ -1,14 +1,46 @@
-const Ogle = require('./Ogle');
+const Ogle = require("./Ogle");
 
 let ogle;
-describe('Ogle', () => {
-  beforeEach(() => {
-    ogle = new Ogle();
-  });
-  afterEach(() => {
-    ogle = null;
-  });
 
-  it('is an instance of `Ogle`', () => expect(ogle).toBeInstanceOf(Ogle));
-  it('is defined', () => expect(ogle).toBeDefined());
+const options = {
+  base: "https://home.ti.dev/",
+  test: "https://home.ti.dev/?enableBetaLogo=true"
+};
+const nullit = () => {
+  ogle = null;
+};
+
+describe("Ogle", () => {
+  afterEach(nullit);
+  describe("with invalid params", () => {
+    it("should throw error", () => {
+      expect(() => {
+        ogle = new Ogle();
+      }).toThrow();
+    });
+  });
+  describe("with valid params", () => {
+    beforeEach(() => {
+      ogle = new Ogle(options);
+    });
+    it("is an instance of `Ogle`", () => expect(ogle).toBeInstanceOf(Ogle));
+    it("is defined", () => expect(ogle).toBeDefined());
+
+    describe("#constructor", () => {
+      it("sets @base", () => expect(ogle.base).toBe("https://home.ti.dev/"));
+      it("sets @test", () =>
+        expect(ogle.test).toBe("https://home.ti.dev/?enableBetaLogo=true"));
+    });
+
+    describe("#capture", () => {
+      it("is defined", () => expect(ogle.capture).toBeDefined());
+    });
+  });
+});
+
+this.nightmare = new Nightmare({
+  show: true,
+  switches: {
+    "ignore-certificate-errors": true
+  }
 });
