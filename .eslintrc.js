99539cf085531ccc5d5b3ac5bd329fe06bd60ab8
Zachary Abresch <zachary.abresch@gmail.com>
2018-07-11 22:55:06 +0000

    Reimplement build with `webpack`

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,11 @@
 module.exports = {
   extends: ['airbnb-base', 'prettier'],
   env: {
+    node: true,
+    es6: true,
     jest: true
   },
+  plugins: ['prettier'],
   rules: {
     'no-console': 'off',
     quotes: ['error', 'single'],
