187e1826265415243a5c976051d79964d095de92
Zachary Abresch <zachary.abresch@gmail.com>
2018-07-13 21:04:16 +0000

    Update README & add github templates

--- src/example.js
+++ src/example.js
@@ -1,11 +1,7 @@
-const { exec } = require('child_process');
 const Ogle = require('./Ogle');
 
 const options = {
-  urls: {
-    base: 'https://home.ti.dev/',
-    test: 'https://home.ti.dev/?enableBetaLogo=true'
-  }
+  urls: { base: 'https://google.com', test: 'https://google.jp' }
 };
 
 // const options = {
@@ -21,6 +17,5 @@ ogle
   .capture()
   .then(() => {
     console.log('well done!');
-    exec('imgcat ./ogle/images/diff.png');
   })
   .catch(err => console.error(err));
